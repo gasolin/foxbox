51597d016e42a9fd476f7d80ef2d1f10e1d0237e
gasolin <gasolin@gmail.com>
2014-01-29 10:49:54 +0800

    add ASCII banner and fix path

--- configure.sh
+++ configure.sh
@@ -1,12 +1,26 @@
-# get repo
+#
+# foxbox auto setup script
+#
+# author: fred lin (gasolin@gmail.com)
+#
+echo "███████╗ ██████╗ ██╗  ██╗██████╗  ██████╗ ██╗  ██╗"
+echo "██╔════╝██╔═══██╗╚██╗██╔╝██╔══██╗██╔═══██╗╚██╗██╔╝"
+echo "█████╗  ██║   ██║ ╚███╔╝ ██████╔╝██║   ██║ ╚███╔╝ "
+echo "██╔══╝  ██║   ██║ ██╔██╗ ██╔══██╗██║   ██║ ██╔██╗ "
+echo "██║     ╚██████╔╝██╔╝ ██╗██████╔╝╚██████╔╝██╔╝ ██╗"
+echo "╚═╝      ╚═════╝ ╚═╝  ╚═╝╚═════╝  ╚═════╝ ╚═╝  ╚═╝"
+echo "      FirefoxOS Build Environment in a VM         "
+
+echo "get repo"
 curl http://commondatastorage.googleapis.com/git-repo-downloads/repo > ./repo
 chmod a+x ./repo
 
-# get B2G
+echo "get B2G"
 mkdir B2G
 cd B2G
-./repo init -u https://github.com/mozilla-b2g/B2G.git
-./repo sync
+../repo init -u https://github.com/mozilla-b2g/B2G.git
+../repo sync
+cd ..
 
-# start vagrant
+echo "start vagrant with ${PWD}/B2G"
 B2G_PATH=${PWD}/B2G vagrant up
