b0f4e7ec5773c1c3ebfafbaa79c23d481a5a1564
gasolin <gasolin@gmail.com>
2014-03-19 10:12:17 +0800

    add commented GECKO/GAIA_PATH

--- reload.sh
+++ reload.sh
@@ -13,3 +13,5 @@ echo "      FirefoxOS Build Environment in a VM         "
 
 echo "      Reload the script and provisioning again    "
 B2G_PATH=${PWD}/B2G vagrant reload --provision
+#GECKO_PATH=${PWD}/gecko vagrant reload --provision
+#GAIA_PATH=${PWD}/gaia vagrant reload --provision
