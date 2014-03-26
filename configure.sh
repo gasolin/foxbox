9124c0ed984cdb2f28dd4833ccf1162d7096b8c4
gasolin <gasolin@gmail.com>
2014-03-26 12:23:41 +0800

    add platform detection script while configure

--- configure.sh
+++ configure.sh
@@ -15,6 +15,72 @@ echo "      FirefoxOS Build Environment in a VM         "
 #curl http://commondatastorage.googleapis.com/git-repo-downloads/repo > ./repo
 #chmod a+x ./repo
 
-sudo apt-get install -y dkms nfs-kernel-server nfs-common
+# platform detection from http://stackoverflow.com/questions/394230/detect-the-os-from-a-bash-script
+lowercase(){
+    echo "$1" | sed "y/ABCDEFGHIJKLMNOPQRSTUVWXYZ/abcdefghijklmnopqrstuvwxyz/"
+}
+
+OS=`lowercase \`uname\``
+KERNEL=`uname -r`
+MACH=`uname -m`
+
+if [ "{$OS}" == "windowsnt" ]; then
+    OS=windows
+elif [ "{$OS}" == "darwin" ]; then
+    OS=mac
+else
+    OS=`uname`
+    if [ "${OS}" = "SunOS" ] ; then
+        OS=Solaris
+        ARCH=`uname -p`
+        OSSTR="${OS} ${REV}(${ARCH} `uname -v`)"
+    elif [ "${OS}" = "AIX" ] ; then
+        OSSTR="${OS} `oslevel` (`oslevel -r`)"
+    elif [ "${OS}" = "Linux" ] ; then
+        if [ -f /etc/redhat-release ] ; then
+            DistroBasedOn='RedHat'
+            DIST=`cat /etc/redhat-release |sed s/\ release.*//`
+            PSUEDONAME=`cat /etc/redhat-release | sed s/.*\(// | sed s/\)//`
+            REV=`cat /etc/redhat-release | sed s/.*release\ // | sed s/\ .*//`
+        elif [ -f /etc/SuSE-release ] ; then
+            DistroBasedOn='SuSe'
+            PSUEDONAME=`cat /etc/SuSE-release | tr "\n" ' '| sed s/VERSION.*//`
+            REV=`cat /etc/SuSE-release | tr "\n" ' ' | sed s/.*=\ //`
+        elif [ -f /etc/mandrake-release ] ; then
+            DistroBasedOn='Mandrake'
+            PSUEDONAME=`cat /etc/mandrake-release | sed s/.*\(// | sed s/\)//`
+            REV=`cat /etc/mandrake-release | sed s/.*release\ // | sed s/\ .*//`
+        elif [ -f /etc/debian_version ] ; then
+            DistroBasedOn='Debian'
+            DIST=`cat /etc/lsb-release | grep '^DISTRIB_ID' | awk -F=  '{ print $2 }'`
+            PSUEDONAME=`cat /etc/lsb-release | grep '^DISTRIB_CODENAME' | awk -F=  '{ print $2 }'`
+            REV=`cat /etc/lsb-release | grep '^DISTRIB_RELEASE' | awk -F=  '{ print $2 }'`
+        fi
+        if [ -f /etc/UnitedLinux-release ] ; then
+            DIST="${DIST}[`cat /etc/UnitedLinux-release | tr "\n" ' ' | sed s/VERSION.*//`]"
+        fi
+        OS=`lowercase $OS`
+        DistroBasedOn=`lowercase $DistroBasedOn`
+        readonly OS
+        readonly DIST
+        readonly DistroBasedOn
+        readonly PSUEDONAME
+        readonly REV
+        readonly KERNEL
+        readonly MACH
+    fi
+
+fi
+
+# shootProfile
+echo "OS: $OS"
+echo "DIST: $DIST"
+echo "REV: $REV"
+echo "DistroBasedOn: $DistroBasedOn"
+
+if [ "$DIST" == "Ubuntu" ]; then
+    sudo apt-get install -y dkms nfs-kernel-server nfs-common
+fi
+
 echo "start vagrant with ${PWD}/B2G"
 B2G_PATH=${PWD}/B2G vagrant up
